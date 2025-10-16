155b5aa6632ed09483ecad113322047b4cb97d17
dayani <mkdkdesilva@gmail.com>
2025-10-16 08:32:37 +0530

    Improve UI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,6 +66,8 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
 
+    implementation("com.google.android.material:material:1.12.0")
+
     // Testing
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
